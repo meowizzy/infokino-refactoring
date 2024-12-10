2a4a8d3428aa147d178e44077835845365a558b5
Maksad Khalipaev <maks14flay@gmail.com>
2024-12-10 18:07:05 +0500

    test eslint error husky

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 
 export const App = () => {
   const [counter, setCounter] = useState(0);
-  const test = 121;
+  const test = 123131;
 
   const onClick = () => {
     setCounter(counter + 1);
