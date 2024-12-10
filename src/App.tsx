8d443ad315e2e3ecb7a470f6bde113039872a98b
Maksad Khalipaev <maks14flay@gmail.com>
2024-12-10 18:38:00 +0500

    wadadadadada

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 
 export const App = () => {
   const [counter, setCounter] = useState(0);
-  const awdawdwa: string = 1233131;
 
   const onClick = () => {
     setCounter(counter + 1);
@@ -11,7 +10,6 @@ export const App = () => {
 
   return (
     <>
-      {awdawdwa}
       <h1>{counter}</h1>
       <Button onClick={onClick}>Увеличить счетч2222ик</Button>
     </>
