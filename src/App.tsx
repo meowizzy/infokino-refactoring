f57988bb18b7d2301b1b1ce667e66ab467049965
Maksad Khalipaev <maks14flay@gmail.com>
2024-12-12 23:53:37 +0500

    IT'S WORK

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 
 export const App = () => {
   const [counter, setCounter] = useState(0);
-  const test = 121
 
   const onClick = () => {
     setCounter(counter + 1);
